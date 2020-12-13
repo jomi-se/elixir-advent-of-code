987e8fe0ee6e5983266b025b039210a5107be47f
jomi-se <jose.m.arroyo.se@gmail.com>
2020-12-13 21:53:20 +0100

    Make solution more readable

--- lib/13.ex
+++ lib/13.ex
@@ -44,54 +44,45 @@ defmodule Buses do
   def part_2 do
     buses = read_file_2!()
 
-    [head | tail] = buses
+    buses
     |> Enum.sort_by(fn {elem, _} -> -elem end)
+    |> find_common_step_for_all()
+    |> find_smallest_timestamp()
 
-    {mult, step} = find_common_step(head, tail, 1, 1)
-    find_lowest_timestamp(head, tail, mult, step)
   end
 
-  def find_lowest_timestamp(head, tail, mult, step) do
-    {first_id, f_index} = head
-    target_timestamp = first_id * mult - f_index
-    IO.inspect({mult, step})
-
-    is_solution = Enum.all?(tail, fn ({id, index}) -> rem(target_timestamp + index, id) == 0 end) && mult > 0
-    case is_solution do
-      false -> first_id * (mult + step) - f_index
-      true -> find_lowest_timestamp(head, tail, mult - step, step)
-      end
+  def find_smallest_timestamp({largest_bus, mult, step}) do
+    smallest_mult = rem(mult, step)
+    {id, index} = largest_bus
+    id * smallest_mult - index
   end
 
-  def find_common_step(_, [], mult, step) do
-    {mult,step}
+  def find_common_step_for_all([largest_bus | tail]) do
+   {mult, step} = tail
+    |> Enum.reduce({1, 1}, fn (bus, {mult, step})->
+      find_common_step_2(largest_bus, bus, mult, step)
+      |> IO.inspect()
+   end)
+
+   {largest_bus, mult, step}
   end
 
-  def find_common_step(first, [head | buses], mult, step) do
-    {first_id, f_index} = first
-    target_timestamp = first_id * mult - f_index
+  def find_common_step_2(bus_a, bus_b, mult, step) do
+    mult_start = find_first_matching(bus_a, bus_b, mult, step)
+    mult_end = find_first_matching(bus_a, bus_b, mult_start + step, step)
+    new_step = mult_end - mult_start
+    {mult_end, new_step}
+  end
 
-    {bus_id, h_index} = head
-    IO.inspect({first_id, bus_id, mult, step, rem(target_timestamp + h_index, bus_id)})
-    match = rem(target_timestamp + h_index, bus_id) == 0
-    if match do
-      find_common_step(first, [head| buses], mult + step, step, mult)
-    else
-      find_common_step(first, [head| buses], mult + step, step)
+  def find_first_matching(bus_a, bus_b, mult, step) do
+    {a_id, a_index} = bus_a
+    {b_id, b_index} = bus_b
+    timestamp_0 = a_id * mult - a_index
+    is_match = rem(timestamp_0 + b_index, b_id) == 0
+    case is_match do
+        true -> mult
+        false -> find_first_matching(bus_a, bus_b, mult + step, step)
     end
   end
 
-  def find_common_step(first, [head | buses], mult, step, prev_mult_match) do
-    {first_id, f_index} = first
-    target_timestamp = first_id * mult - f_index
-
-    {bus_id, h_index} = head
-    IO.inspect({first_id, bus_id, mult, step, rem(target_timestamp + h_index, bus_id)})
-    match = rem(target_timestamp + h_index, bus_id) == 0
-    if match do
-      find_common_step(first, buses, mult, mult - prev_mult_match)
-    else
-      find_common_step(first, [head| buses], mult + step, step, prev_mult_match)
-    end
-  end
 end
