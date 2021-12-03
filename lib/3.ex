7f69d80be5d01380e7e860fc69233df1e6ad9d92
jomi-se <jose.m.arroyo.se@gmail.com>
2021-12-03 11:42:21 -0300

    Day 3 part 2

--- lib/3.ex
+++ lib/3.ex
@@ -75,4 +75,82 @@ defmodule SubmarineConsumption do
 
     {gamma * epsilon, gamma, epsilon}
   end
+
+  def transpose(list_of_list) do
+    Enum.zip(list_of_list) |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def get_o2_significant_bit(list, len) do
+    sum = list |> Enum.sum()
+
+    cond do
+      sum >= len / 2 ->
+        1
+
+      true ->
+        0
+    end
+  end
+
+  def get_o2([pick],_), do: pick
+  def get_o2(num_list, pos) do
+    len = length(num_list)
+    digit_list = transpose(num_list)
+    sig_dig_list  = Enum.at(digit_list, pos)
+
+    sig_bit =
+      get_o2_significant_bit(sig_dig_list, len)
+
+    num_list
+    |> Enum.filter(fn num ->
+      sig_bit == Enum.at(num, pos)
+    end)
+    |> get_o2(pos + 1)
+  end
+
+  def get_co_significant_bit(list, len) do
+    sum = list |> Enum.sum()
+
+    cond do
+      sum >= len / 2 ->
+        0
+
+      true ->
+        1
+    end
+  end
+
+  def get_co([pick],_), do: pick
+  def get_co(num_list, pos) do
+    len = length(num_list)
+    digit_list = transpose(num_list)
+    sig_dig_list  = Enum.at(digit_list, pos)
+
+    sig_bit =
+      get_co_significant_bit(sig_dig_list, len)
+
+    num_list
+    |> Enum.filter(fn num ->
+      sig_bit == Enum.at(num, pos)
+    end)
+    |> get_co(pos + 1)
+  end
+
+  def solve2() do
+    num_list = read_file()
+
+    o2 = get_o2(num_list, 0)
+      |> Enum.join()
+      |> IO.inspect()
+      |> Integer.parse(2)
+      |> elem(0)
+
+    co = get_co(num_list, 0)
+      |> Enum.join()
+      |> IO.inspect()
+      |> Integer.parse(2)
+      |> elem(0)
+
+    {co * o2, co, o2}
+  end
 end
