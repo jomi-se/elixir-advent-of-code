42149a2b1f3fd1c37140f276780f220fbca9d601
jomi-se <jose.m.arroyo.se@gmail.com>
2022-12-21 10:45:04 +0100

    Solve part 2

--- years/aoc2022/lib/21.ex
+++ years/aoc2022/lib/21.ex
@@ -9,7 +9,7 @@ defmodule MonkeyNums do
 
       cond do
         Regex.match?(~r/^\d+$/, rest) ->
-          {name, %{:num => String.to_integer(rest)}}
+          {name, %{:num => String.to_integer(rest), :name => name, :has_humn => name == "humn"}}
 
         true ->
           %{"left" => left, "op" => op, "right" => right} =
@@ -33,7 +33,7 @@ defmodule MonkeyNums do
                 fn l, r -> div(l, r) end
             end
 
-          {name, %{:left => left, :right => right, :fun => fun}}
+          {name, %{:left => left, :right => right, :fun => fun, :op => op, :name => name}}
       end
     end)
     |> Map.new()
@@ -55,10 +55,77 @@ defmodule MonkeyNums do
     end
   end
 
+  def has_humn(name, map) do
+    monkey = Map.fetch!(map, name)
+
+    case Map.get(monkey, :num, nil) do
+      nil ->
+        {has_left, left_map} = has_humn(monkey.left, map)
+        {has_right, right_map} = has_humn(monkey.right, left_map)
+
+        has = has_left or has_right
+        new_monkey = Map.put(monkey, :has_humn, has)
+        {has, Map.put(right_map, name, new_monkey)}
+
+      _ ->
+        {monkey.has_humn, map}
+    end
+  end
+
   def solve1 do
     monkey_map = read_file()
 
     {root_num, _new_monkey_map} = get_monkey_val("root", monkey_map)
     root_num
+  end
+
+  def compute_new_base_num(base_num, other_num, "+", _), do: base_num - other_num
+  def compute_new_base_num(base_num, other_num, "*", _), do: div(base_num, other_num)
+  def compute_new_base_num(base_num, other_num, "-", :right), do: base_num + other_num
+  def compute_new_base_num(base_num, other_num, "-", :left), do: other_num - base_num
+  def compute_new_base_num(base_num, other_num, "/", :right), do: base_num * other_num
+  def compute_new_base_num(base_num, other_num, "/", :left), do: div(other_num, base_num)
+
+  def compute_humn_value(base_num, "humn", _monkey_map), do: base_num
+
+  def compute_humn_value(base_num, name, monkey_map) do
+    monkey = Map.fetch!(monkey_map, name)
+
+    left_monkey = Map.fetch!(monkey_map, monkey.left)
+    right_monkey = Map.fetch!(monkey_map, monkey.right)
+
+    cond do
+      left_monkey.has_humn ->
+        {base_right, new_monkey_map} = get_monkey_val(right_monkey.name, monkey_map)
+        new_base_num = compute_new_base_num(base_num, base_right, monkey.op, :right)
+        compute_humn_value(new_base_num, left_monkey.name, new_monkey_map)
+
+      right_monkey.has_humn ->
+        {base_left, new_monkey_map} = get_monkey_val(left_monkey.name, monkey_map)
+        new_base_num = compute_new_base_num(base_num, base_left, monkey.op, :left)
+        compute_humn_value(new_base_num, right_monkey.name, new_monkey_map)
+    end
+  end
+
+  def solve2 do
+    monkey_map = read_file()
+
+    root_monkey = Map.fetch!(monkey_map, "root")
+    {has_left, left_map} = has_humn(root_monkey.left, monkey_map)
+    {has_right, right_map} = has_humn(root_monkey.right, left_map)
+    IO.inspect({has_left, has_right})
+
+    {monkey_to_compute, monkey_with_humn} =
+      cond do
+        has_left ->
+          {root_monkey.right, root_monkey.left}
+
+        has_right ->
+          {root_monkey.left, root_monkey.right}
+      end
+
+    {base_num, new_monkey_map} = get_monkey_val(monkey_to_compute, right_map)
+
+    compute_humn_value(base_num, monkey_with_humn, new_monkey_map)
   end
 end
