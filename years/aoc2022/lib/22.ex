425dc23bb648be30b8804c1efdf726967e31c25c
jomi-se <jose.m.arroyo.se@gmail.com>
2022-12-23 00:37:38 +0100

    Solve 22
    Holy shit, how do you even solve that without hardcoding the wrap arounds

--- years/aoc2022/lib/22.ex
+++ years/aoc2022/lib/22.ex
@@ -39,39 +39,368 @@ defmodule PathPass do
     parsed_instructions =
       instructions
       |> String.trim("\n")
-      |> String.graphemes()
-      |> parse_instructions([])
+      |> String.split(~r/(\d+)/, include_captures: true, trim: true)
+      |> Enum.map(fn
+        char when char in ["L", "R"] -> char
+        num -> String.to_integer(num)
+      end)
 
     {parsed_map, parsed_instructions}
   end
 
-  def get_char([char | rest]) when char in ["R", "L"], do: {char, [char|rest]}
+  def get_next_dir("N", "R"), do: "E"
+  def get_next_dir("E", "R"), do: "S"
+  def get_next_dir("S", "R"), do: "W"
+  def get_next_dir("W", "R"), do: "N"
 
-  def get_num(char_list, acc \\ [])
+  def get_next_dir("E", "L"), do: "N"
+  def get_next_dir("S", "L"), do: "E"
+  def get_next_dir("W", "L"), do: "S"
+  def get_next_dir("N", "L"), do: "W"
 
-  def get_num([num | rest], acc) when num in ["R", "L"],
-    do: {acc |> Enum.reverse() |> Enum.join("") |> String.to_integer(), [num|rest]}
+  def step_coords("N"), do: {0, -1}
+  def step_coords("S"), do: {0, 1}
+  def step_coords("E"), do: {1, 0}
+  def step_coords("W"), do: {-1, 0}
 
-  def get_num([num | rest], acc), do: get_num(rest, [num | acc])
+  def wrap_around(pos, cur_dir, map)
+  def wrap_around({x, y}, "E", map) when x > map.max_x, do: wrap_around({map.min_x, y}, "E", map)
 
-  def parse_instructions([], acc), do: acc |> Enum.reverse()
+  def wrap_around({x, y}, "E", map) do
+    case Map.get(map, {x, y}, nil) do
+      nil ->
+        wrap_around({x + 1, y}, "E", map)
 
-  def parse_instructions(char_list, acc) do
-    IO.inspect(binding())
-    {num, rest_chars} = get_num(char_list)
-    |> IO.inspect()
+      "#" ->
+        nil
 
-    case rest_chars do
-      [] ->
-        [num | acc] |> Enum.reverse()
-
-      _ ->
-        {char, rest_chars2} = get_char(rest_chars)
-        parse_instructions(rest_chars2, [char, num | acc])
+      "." ->
+        {x, y}
     end
   end
 
+  def wrap_around({x, y}, "W", map) when x < map.min_x, do: wrap_around({map.max_x, y}, "W", map)
+
+  def wrap_around({x, y}, "W", map) do
+    case Map.get(map, {x, y}, nil) do
+      nil ->
+        wrap_around({x - 1, y}, "W", map)
+
+      "#" ->
+        nil
+
+      "." ->
+        {x, y}
+    end
+  end
+
+  def wrap_around({x, y}, "S", map) when y > map.max_y, do: wrap_around({x, map.min_y}, "S", map)
+
+  def wrap_around({x, y}, "S", map) do
+    case Map.get(map, {x, y}, nil) do
+      nil ->
+        wrap_around({x, y + 1}, "S", map)
+
+      "#" ->
+        nil
+
+      "." ->
+        {x, y}
+    end
+  end
+
+  def wrap_around({x, y}, "N", map) when y < map.min_y, do: wrap_around({x, map.max_y}, "N", map)
+
+  def wrap_around({x, y}, "N", map) do
+    case Map.get(map, {x, y}, nil) do
+      nil ->
+        wrap_around({x, y - 1}, "N", map)
+
+      "#" ->
+        nil
+
+      "." ->
+        {x, y}
+    end
+  end
+
+  def get_next_pos({x, y}, _cur_dir, 0, _map), do: {x, y}
+
+  def get_next_pos({x, y}, cur_dir, steps, map) do
+    {x1, y1} = step_coords(cur_dir)
+
+    {x2, y2} = {x + x1, y + y1}
+
+    case Map.get(map, {x2, y2}, nil) do
+      nil ->
+        # wrap around
+        case wrap_around({x2, y2}, cur_dir, map) do
+          nil ->
+            # blocked
+            {x, y}
+
+          new_pos ->
+            get_next_pos(new_pos, cur_dir, steps - 1, map)
+        end
+
+      "#" ->
+        # blocked
+        {x, y}
+
+      "." ->
+        # take step
+        get_next_pos({x2, y2}, cur_dir, steps - 1, map)
+    end
+  end
+
+  def do_move(pos, cur_dir, instructions, map)
+
+  def do_move(pos, cur_dir, [], _map),
+    do: {pos, cur_dir}
+
+  def do_move({x, y}, cur_dir, ["R" | rest], map),
+    do: do_move({x, y}, get_next_dir(cur_dir, "R"), rest, map)
+
+  def do_move({x, y}, cur_dir, ["L" | rest], map),
+    do: do_move({x, y}, get_next_dir(cur_dir, "L"), rest, map)
+
+  def do_move({x, y}, cur_dir, [instr | rest], map) when is_integer(instr) do
+    do_move(get_next_pos({x, y}, cur_dir, instr, map), cur_dir, rest, map)
+  end
+
+  def get_dir_value("E"), do: 0
+  def get_dir_value("S"), do: 1
+  def get_dir_value("W"), do: 2
+  def get_dir_value("N"), do: 3
+
   def solve1 do
-    read_file()
+    {parsed_map, parsed_instructions} = read_file()
+
+    start_pos = wrap_around({1, 1}, "E", parsed_map)
+
+    {{x, y}, last_dir} =
+      do_move(start_pos, "E", parsed_instructions, parsed_map)
+      |> IO.inspect()
+
+    y * 1000 + x * 4 + get_dir_value(last_dir)
+  end
+
+
+  ##
+  def wrap_around_2({x,y}, "E", map) when x == 150 and y <= 50 and y >= 1 do
+    new_pos = {100, 151 - y}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "W"}
+    end
+  end
+  def wrap_around_2({x,y}, "E", map) when x == 100 and y <= 150 and y >= 101 do
+    new_pos = {150, 151 - y}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "W"}
+    end
+  end
+  ##
+
+  ##
+  def wrap_around_2({x,y}, "N", map) when x <= 150  and x >= 101 and y == 1 do
+    new_pos = {x-100, 200}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      ->  {new_pos, "N"}
+    end
+  end
+  def wrap_around_2({x,y}, "S", map) when x <= 50  and x >= 1 and y == 200 do
+    new_pos = {x+100, 1}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "S"}
+    end
+  end
+  ##
+
+  ##
+  def wrap_around_2({x,y}, "N", map) when x <= 100  and x >= 51 and y == 1 do
+    new_pos = {1, 100+x}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "E"}
+    end
+  end
+  def wrap_around_2({x,y}, "W", map) when x == 1  and y >= 151 and y <= 200 do
+    new_pos = {y-100, 1}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "S"}
+    end
+  end
+  ##
+
+  ##
+  def wrap_around_2({x,y}, "W", map) when x == 51 and y >= 1 and y <= 50 do
+    new_pos = {1, 151 - y}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "E"}
+    end
+  end
+  def wrap_around_2({x,y}, "W", map) when x == 1 and y >= 101 and y <= 150 do
+    new_pos = {51, 151-y}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "E"}
+    end
+  end
+  ##
+
+  ##
+  def wrap_around_2({x,y}, "W", map) when x == 51 and y >= 51 and y <= 100 do
+    new_pos = {y-50, 101}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "S"}
+    end
+  end
+  def wrap_around_2({x,y}, "N", map) when x >= 1 and x <= 50 and y == 101 do
+    new_pos = {51, x+50}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "E"}
+    end
+  end
+  ##
+
+  ##
+  def wrap_around_2({x,y}, "S", map) when x >= 101 and x <= 150 and y == 50 do
+    new_pos = {100, x - 50}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "W"}
+    end
+  end
+  def wrap_around_2({x,y}, "E", map) when x == 100 and y >= 51 and y <= 100 do
+    new_pos = {50+y, 50}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "N"}
+    end
+  end
+  ##
+
+  ##
+  def wrap_around_2({x,y}, "S", map) when x >= 51 and x <= 100 and y == 150 do
+    new_pos = {50, x + 100}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "W"}
+    end
+  end
+  def wrap_around_2({x,y}, "E", map) when x == 50 and y >= 151 and y <= 200 do
+    new_pos = {y-100, 150}
+
+    case Map.get(map, new_pos, nil) do
+      "#" -> nil
+
+      "."
+      -> {new_pos, "N"}
+    end
+  end
+  ##
+
+  def get_next_pos2({x, y}, cur_dir, 0, _map), do: {{x, y}, cur_dir}
+
+  def get_next_pos2({x, y}, cur_dir, steps, map) do
+    {x1, y1} = step_coords(cur_dir)
+
+    {x2, y2} = {x + x1, y + y1}
+
+    case Map.get(map, {x2, y2}, nil) do
+      nil ->
+        # wrap around
+        case wrap_around_2({x, y}, cur_dir, map) do
+          nil ->
+            # blocked
+            {{x, y}, cur_dir}
+
+          {new_pos, new_dir} ->
+            get_next_pos2(new_pos, new_dir, steps - 1, map)
+        end
+
+      "#" ->
+        # blocked
+        {{x, y}, cur_dir}
+
+      "." ->
+        # take step
+        get_next_pos2({x2, y2}, cur_dir, steps - 1, map)
+    end
+  end
+
+  def do_move_2(pos, cur_dir, instructions, map)
+
+  def do_move_2(pos, cur_dir, [], _map),
+    do: {pos, cur_dir}
+
+  def do_move_2({x, y}, cur_dir, ["R" | rest], map),
+    do: do_move_2({x, y}, get_next_dir(cur_dir, "R"), rest, map)
+
+  def do_move_2({x, y}, cur_dir, ["L" | rest], map),
+    do: do_move_2({x, y}, get_next_dir(cur_dir, "L"), rest, map)
+
+  def do_move_2({x, y}, cur_dir, [instr | rest], map) when is_integer(instr) do
+    {next_pos,next_dir} = get_next_pos2({x, y}, cur_dir, instr, map)
+    do_move_2(next_pos, next_dir, rest, map)
+  end
+
+  def solve2 do
+    {parsed_map, parsed_instructions} = read_file()
+
+    start_pos = wrap_around({1, 1}, "E", parsed_map)
+
+    {{x, y}, last_dir} =
+      do_move_2(start_pos, "E", parsed_instructions, parsed_map)
+      |> IO.inspect()
+
+    y * 1000 + x * 4 + get_dir_value(last_dir)
   end
 end
